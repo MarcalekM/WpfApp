58883296372c327b42c5cfb8db2fe6b29f5738d8
Sufi <120452154+MarcalekM@users.noreply.github.com>
2024-03-01 11:22:39 +0100

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            lbx1.Items.Add("Csütörtök");
+            lbx1.Items.Add("Péntek");
+            lbx1.Items.Add("Szombat");
+            lbx1.Items.Add("Vasárnap");
+        }
+    }
+}
